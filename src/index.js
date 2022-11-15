ed1a236097959c90d7f1996d1f2079f38c086118
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-11-15 14:53:16 +0200

    react tutorial

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-import React , { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+
 import './index.css'
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
-  }
-}
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
